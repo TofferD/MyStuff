50667a9d69fe352237ce7d62efbda15d4caa9bbe
TofferD <toffer@gmail.com>
2018-03-13 13:04:18 -0700

    Adding new a new class for the encode and decode tiny url problem from leetcode and a new test case to use it.

---
+++ LeetCodeStuff/LeetCodeStuffTests/EncodeDecodeTinyUrlTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeStuff;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeStuffTests
+{
+    [TestClass]
+    public class EncodeDecodeTinyUrlTests
+    {
+        [TestMethod]
+        public void TestForEmptyOrNullUrl()
+        {
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            Assert.IsTrue(testObj.decode(testObj.encode(string.Empty)) == string.Empty);
+            Assert.IsTrue(testObj.decode(testObj.encode(null)) == string.Empty);
+        }
+    }
+}
