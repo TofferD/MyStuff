8e2c03ea88fe1f18a8a0a723b3a75ee14bf5692d
TofferD <toffer@gmail.com>
2018-03-13 20:40:04 -0700

    this is adding new test cases and fixing bugs in the encode and decode methods.

--- LeetCodeStuff/LeetCodeStuffTests/EncodeDecodeTinyUrlTests.cs
+++ LeetCodeStuff/LeetCodeStuffTests/EncodeDecodeTinyUrlTests.cs
@@ -7,12 +7,72 @@ namespace LeetCodeStuffTests
     public class EncodeDecodeTinyUrlTests
     {
         [TestMethod]
-        public void TestForEmptyOrNullUrl()
+        public void EncodeEmptyOrNullUrlReturnsEmptyStringTest()
         {
             EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
 
-            Assert.IsTrue(testObj.decode(testObj.encode(string.Empty)) == string.Empty);
-            Assert.IsTrue(testObj.decode(testObj.encode(null)) == string.Empty);
+            Assert.IsTrue(testObj.encode(string.Empty) == string.Empty);
+            Assert.IsTrue(testObj.encode(null) == string.Empty);
+        }
+
+        [TestMethod]
+        public void DecodeEmptyOrNullUrlReturnsEmptyStringTest()
+        {
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            Assert.IsTrue(testObj.decode(string.Empty) == string.Empty);
+            Assert.IsTrue(testObj.decode(null) == string.Empty);
+        }
+
+        [TestMethod]
+        public void UrlIsShorterOrEqualThanTinyReturnsOriginalUrlTest()
+        {
+            string littleUrl = "a";
+            string mediumUrl = "bbbbbbbbbbbbbbbbbbbbbbbbbbb";
+
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            string littleResponse = testObj.encode(littleUrl);
+            string mediumResponse = testObj.encode(mediumUrl);
+
+            Assert.IsTrue(littleResponse == littleUrl);
+            Assert.IsTrue(mediumResponse == mediumUrl);
+        }
+
+        [TestMethod]
+        public void UrlIsLongerThanTinyReturnsTinyUrlTest()
+        {
+            string bigUrl = "cccccccccccccccccccccccccccc";
+            string superBigUrl = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            string bigResponse = testObj.encode(bigUrl);
+            string superBigResponse = testObj.encode(superBigUrl);
+
+            Assert.IsTrue(bigResponse != bigUrl);
+            Assert.IsTrue(superBigResponse != superBigUrl);
+        }
+
+        [TestMethod]
+        public void DecodedTinyReturnsOriginalUrlTest()
+        {
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            string superBigUrl = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+            string superBigResponse = testObj.decode(testObj.encode(superBigUrl));
+
+            Assert.IsTrue(superBigResponse == superBigUrl);
+        }
+
+        [TestMethod]
+        public void DecodedInvalidTinyReturnsEmptyStringTest()
+        {
+            EncodeDecodeTinyUrl testObj = new EncodeDecodeTinyUrl();
+
+            string superBigResponse = testObj.decode("somecrazykeythatdoesnotexist");
+
+            Assert.IsTrue(superBigResponse == string.Empty);
         }
     }
 }
